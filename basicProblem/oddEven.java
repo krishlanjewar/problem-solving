0de75ec3cca04ac73f1c9cb1de2a69bc043dff90
krish lanjewar <lanjewarkrish07@gmail.com>
2025-06-22 08:23:47 +0530

    Add initial implementation of oddEven class to determine if a number is odd or even

---
+++ basicProblem/oddEven.java
@@ -0,0 +1,13 @@
+public class oddEven {
+
+    public static void main(String[] args) {
+        
+        int a =8;
+        if ( a%2==0){
+            System.out.println("even");
+        }
+        else{
+            System.out.println("odd");
+        }
+    }
+}
