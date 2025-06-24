f5640179d5ca9facc544feb838afa00636971e27
krish lanjewar <lanjewarkrish07@gmail.com>
2025-06-24 20:22:56 +0530

    Refactor oddEven class to prompt user for input and determine if the number is odd or even

--- basicProblem/oddEven.java
+++ basicProblem/oddEven.java
@@ -1,13 +1,21 @@
+import java.util.Scanner;
 public class oddEven {
 
-    public static void main(String[] args) {
-        
-        int a =8;
-        if ( a%2==0){
-            System.out.println("even");
+    public static void main(String[] args) {      
+        Scanner ss = new Scanner(System.in);
+        System.out.println("Enter a +ve number: ");
+        int num = ss.nextInt();
+        if(0<num){
+            if (num % 2 == 0) {
+                System.out.println(num + " : is  even number ");
+            } 
+            else{
+                System.out.println(num + " : is odd number");
+            }
         }
         else{
-            System.out.println("odd");
+            System.out.println("Enter a +ve number");
         }
+           ss.close();
     }
 }
