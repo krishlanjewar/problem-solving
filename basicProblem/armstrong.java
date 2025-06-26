19124404b0bbdf8e126fe30e80131d78f845992f
krish lanjewar <lanjewarkrish07@gmail.com>
2025-06-26 19:51:15 +0530

    Refactor armstrong class to improve code readability by adding whitespace

--- basicProblem/armstrong.java
+++ basicProblem/armstrong.java
@@ -7,17 +7,23 @@ public class armstrong {
         int tem = num ;
         int tem2 = num;
         int count = 0;
+        
+        
         while (num>0) {
              num /= 10;
             count++ ;
         }
-        int digit = 0 ;
+        
+        
+        int digit = 0 ;                    
         while (tem>0) {
             int remainder =tem % 10 ;
             digit = digit + (int) ( Math.pow(remainder ,count ));
             // why  (int) ??
             tem /= 10 ;
         }
+       
+       
         if (tem2 == digit) {
             System.out.println("armstrong");
         } else {
