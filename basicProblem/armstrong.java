3bac9f12904733681457f54502c78c02b9475798
krish lanjewar <lanjewarkrish07@gmail.com>
2025-06-26 19:49:44 +0530

    // why  (int) ??

--- basicProblem/armstrong.java
+++ basicProblem/armstrong.java
@@ -1,6 +1,27 @@
 import java.util.Scanner;
 public class armstrong {
     public static void main(String[] args) {
-        
+        Scanner ss = new Scanner(System.in);
+        System.out.println("Enter a number ;");
+        int num = ss.nextInt();
+        int tem = num ;
+        int tem2 = num;
+        int count = 0;
+        while (num>0) {
+             num /= 10;
+            count++ ;
+        }
+        int digit = 0 ;
+        while (tem>0) {
+            int remainder =tem % 10 ;
+            digit = digit + (int) ( Math.pow(remainder ,count ));
+            // why  (int) ??
+            tem /= 10 ;
+        }
+        if (tem2 == digit) {
+            System.out.println("armstrong");
+        } else {
+            System.out.println("not  amstrong");
+        }
     }
 }
